df3962d721a3c5f6fa685a9b7ec93f8c4ae70b56
Josef <josef.jetmar@gmail.com>
2018-12-17 23:05:56 +0100

    initial robot page

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+﻿import React from 'react'
+
+// destructuring from type
+const Card = ({ name, email, id }) => {
+	return (
+		<div className='bg-light-green dib br3 pa3 ma2 grow shadow-5 tc'>
+			<img alt='robots' src={`https://robohash.org/${id}?200x200`} />
+			<div>
+				<h2>{name}</h2>
+				<p>{email}</p>
+			</div>
+		</div>
+	);
+}
+
+export default Card;
